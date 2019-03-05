c902166ec51102fd545b1f6f88b1672803c47666
yinxiang <894170765@qq.com>
2019-03-05 17:58:50 +0800

    殷翔  5/3

--- src/main/java/com/eastebiz/entity/EbizUser.java
+++ src/main/java/com/eastebiz/entity/EbizUser.java
@@ -1,6 +1,6 @@
 package com.eastebiz.entity;
 
-<<<<<<< HEAD
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -26,79 +26,29 @@ public class EbizUser {
   `Introducer` varchar(10000) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
         **/
 
-      private Integer id;
-      private String userName;
-      private String firstName;
-      private String lastName;
-      private String password;
-      private String email;
-      private String phoneNumber;
-      private String address;
-      private Date   createTime;
-      private Date   updateTime;
-      private String mnote;
-      private String status;
-      private String userType;
-      private String permissions;
-      private String balance;
-      private String personalLimit;
-      private String introducer;
-
-      //关系属性
-      private List<PackageList> packageList  = new ArrayList<PackageList>();
-
-      //get set
-
-=======
-
-
-public class EbizUser {
     private Integer id;
-
     private String userName;
-
     private String firstName;
-
     private String lastName;
-
-    private String companyName;
-
-    private String passWord;
-
-    private String tempPassWord;
-
+    private String password;
     private String email;
-
     private String phoneNumber;
-
     private String address;
-
-    private String createTime;
-
-    private String updateTime;
-
-    private String note;
-
+    private Date createTime;
+    private Date updateTime;
+    private String mnote;
     private String status;
-
     private String userType;
-    //权限
     private String permissions;
+    private String balance;
+    private String personalLimit;
+    private String introducer;
 
-    private Double balance;
-    //个人领取任务限制
-    private Integer personalLimit;
+    //关系属性
+    private List<PackageList> packageList = new ArrayList<PackageList>();
 
-    private String parameterString;
+    //get set
 
-    private String payTimeInfor;
-    //状态
-    private String operatingStatus;
-    //操作记录
-    private String operationRecord;
-    //活跃记录
-    private String activityRecord;
->>>>>>> origin/master
 
     public Integer getId() {
         return id;
@@ -113,11 +63,7 @@ public class EbizUser {
     }
 
     public void setUserName(String userName) {
-<<<<<<< HEAD
         this.userName = userName;
-=======
-        this.userName = userName == null ? null : userName.trim();
->>>>>>> origin/master
     }
 
     public String getFirstName() {
@@ -125,11 +71,7 @@ public class EbizUser {
     }
 
     public void setFirstName(String firstName) {
-<<<<<<< HEAD
         this.firstName = firstName;
-=======
-        this.firstName = firstName == null ? null : firstName.trim();
->>>>>>> origin/master
     }
 
     public String getLastName() {
@@ -137,10 +79,8 @@ public class EbizUser {
     }
 
     public void setLastName(String lastName) {
-<<<<<<< HEAD
         this.lastName = lastName;
     }
-
 
     public String getPassword() {
         return password;
@@ -148,33 +88,6 @@ public class EbizUser {
 
     public void setPassword(String password) {
         this.password = password;
-=======
-        this.lastName = lastName == null ? null : lastName.trim();
-    }
-
-    public String getCompanyName() {
-        return companyName;
-    }
-
-    public void setCompanyName(String companyName) {
-        this.companyName = companyName == null ? null : companyName.trim();
-    }
-
-    public String getPassWord() {
-        return passWord;
-    }
-
-    public void setPassWord(String passWord) {
-        this.passWord = passWord == null ? null : passWord.trim();
-    }
-
-    public String getTempPassWord() {
-        return tempPassWord;
-    }
-
-    public void setTempPassWord(String tempPassWord) {
-        this.tempPassWord = tempPassWord == null ? null : tempPassWord.trim();
->>>>>>> origin/master
     }
 
     public String getEmail() {
@@ -182,12 +95,7 @@ public class EbizUser {
     }
 
     public void setEmail(String email) {
-<<<<<<< HEAD
         this.email = email;
-=======
-
-        this.email = email == null ? null : email.trim();
->>>>>>> origin/master
     }
 
     public String getPhoneNumber() {
@@ -195,11 +103,7 @@ public class EbizUser {
     }
 
     public void setPhoneNumber(String phoneNumber) {
-<<<<<<< HEAD
         this.phoneNumber = phoneNumber;
-=======
-        this.phoneNumber = phoneNumber == null ? null : phoneNumber.trim();
->>>>>>> origin/master
     }
 
     public String getAddress() {
@@ -207,7 +111,6 @@ public class EbizUser {
     }
 
     public void setAddress(String address) {
-<<<<<<< HEAD
         this.address = address;
     }
 
@@ -233,33 +136,6 @@ public class EbizUser {
 
     public void setMnote(String mnote) {
         this.mnote = mnote;
-=======
-        this.address = address == null ? null : address.trim();
-    }
-
-    public String getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(String createTime) {
-        this.createTime = createTime == null ? null : createTime.trim();
-    }
-
-    public String getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(String updateTime) {
-        this.updateTime = updateTime == null ? null : updateTime.trim();
-    }
-
-    public String getNote() {
-        return note;
-    }
-
-    public void setNote(String note) {
-        this.note = note == null ? null : note.trim();
->>>>>>> origin/master
     }
 
     public String getStatus() {
@@ -267,11 +143,7 @@ public class EbizUser {
     }
 
     public void setStatus(String status) {
-<<<<<<< HEAD
         this.status = status;
-=======
-        this.status = status == null ? null : status.trim();
->>>>>>> origin/master
     }
 
     public String getUserType() {
@@ -279,11 +151,7 @@ public class EbizUser {
     }
 
     public void setUserType(String userType) {
-<<<<<<< HEAD
         this.userType = userType;
-=======
-        this.userType = userType == null ? null : userType.trim();
->>>>>>> origin/master
     }
 
     public String getPermissions() {
@@ -291,7 +159,6 @@ public class EbizUser {
     }
 
     public void setPermissions(String permissions) {
-<<<<<<< HEAD
         this.permissions = permissions;
     }
 
@@ -319,8 +186,6 @@ public class EbizUser {
         this.introducer = introducer;
     }
 
-
-
     public List<PackageList> getPackageList() {
         return packageList;
     }
@@ -328,6 +193,8 @@ public class EbizUser {
     public void setPackageList(List<PackageList> packageList) {
         this.packageList = packageList;
     }
+
+  //构造方法
 
 
     public EbizUser() {
@@ -338,8 +205,7 @@ public class EbizUser {
         this.password = password;
     }
 
-
-    public EbizUser(String userName, String firstName, String lastName,  String password, String email, String phoneNumber, String address, Date createTime, Date updateTime, String mnote, String status, String userType, String permissions, String balance, String personalLimit, String introducer) {
+    public EbizUser(String userName, String firstName, String lastName, String password, String email, String phoneNumber, String address, Date createTime, Date updateTime, String mnote, String status, String userType, String permissions, String balance, String personalLimit, String introducer) {
         this.userName = userName;
         this.firstName = firstName;
         this.lastName = lastName;
@@ -357,7 +223,6 @@ public class EbizUser {
         this.personalLimit = personalLimit;
         this.introducer = introducer;
     }
-
 
     public EbizUser(String userName, String firstName, String lastName, String password, String email, String phoneNumber, String address, Date createTime, Date updateTime, String mnote, String status, String userType, String permissions, String balance, String personalLimit, String introducer, List<PackageList> packageList) {
         this.userName = userName;
@@ -379,12 +244,12 @@ public class EbizUser {
         this.packageList = packageList;
     }
 
-    public EbizUser(Integer id, String userName, String firstName, String lastName,  String password, String email, String phoneNumber, String address, Date createTime, Date updateTime, String mnote, String status, String userType, String permissions, String balance, String personalLimit, String introducer, List<PackageList> packageList) {
+
+    public EbizUser(Integer id, String userName, String firstName, String lastName, String password, String email, String phoneNumber, String address, Date createTime, Date updateTime, String mnote, String status, String userType, String permissions, String balance, String personalLimit, String introducer, List<PackageList> packageList) {
         this.id = id;
         this.userName = userName;
         this.firstName = firstName;
         this.lastName = lastName;
-
         this.password = password;
         this.email = email;
         this.phoneNumber = phoneNumber;
@@ -425,65 +290,3 @@ public class EbizUser {
                 '}';
     }
 }
-=======
-        this.permissions = permissions == null ? null : permissions.trim();
-    }
-
-    public Double getBalance() {
-        return balance;
-    }
-
-    public void setBalance(Double balance) {
-        this.balance = balance;
-    }
-
-    public Integer getPersonalLimit() {
-        return personalLimit;
-    }
-
-    public void setPersonalLimit(Integer personalLimit) {
-        this.personalLimit = personalLimit;
-    }
-
-    public String getParameterString() {
-        return parameterString;
-    }
-
-    public void setParameterString(String parameterString) {
-        this.parameterString = parameterString == null ? null : parameterString.trim();
-    }
-
-    public String getPayTimeInfor() {
-        return payTimeInfor;
-    }
-
-    public void setPayTimeInfor(String payTimeInfor) {
-        this.payTimeInfor = payTimeInfor == null ? null : payTimeInfor.trim();
-    }
-
-    public String getOperatingStatus() {
-        return operatingStatus;
-    }
-
-    public void setOperatingStatus(String operatingStatus) {
-        this.operatingStatus = operatingStatus == null ? null : operatingStatus.trim();
-    }
-
-    public String getOperationRecord() {
-        return operationRecord;
-    }
-
-    public void setOperationRecord(String operationRecord) {
-        this.operationRecord = operationRecord == null ? null : operationRecord.trim();
-    }
-
-    public String getActivityRecord() {
-        return activityRecord;
-    }
-
-    public void setActivityRecord(String activityRecord) {
-        this.activityRecord = activityRecord == null ? null : activityRecord.trim();
-    }
-}
-
->>>>>>> origin/master
